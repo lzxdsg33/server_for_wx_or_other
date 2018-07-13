516349844d8302f9e8f3a9a3431b065d71e63fd2
lzxdsg33 <403580791@qq.com>
2018-07-13 18:21:23 +0800

    微信数据模config 模型

---
+++ app/Lib/WxConfigModel.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lzx
+ * Date: 2018/7/13
+ * Time: 17:53
+ * wx配置获取模型
+ */
+
+use Illuminate\Support\Facades\Config;
+
+class WxConfigModel
+{
+    public static $_instance;
+
+    private $_config;
+
+    const WX_APP_ID_INFO = 'wx_app_info';
+    const WX_REQUEST_URL = 'wx_request_url';
+
+    public static function get_instance()
+    {
+        if (!isset(self::$_instance)) {
+            self::$_instance = new self;
+        }
+
+        return self::$_instance;
+    }
+
+    private function __construct()
+    {
+        $this->_config = config('wxInfo');
+    }
+
+    // 基本微信的信息数组
+    public function get_wxInfo()
+    {
+        return $this->_config;
+    }
+
+    // 我的appId
+    public function get_wx_appId()
+    {
+        return $this->_config[self::WX_APP_ID_INFO]['appId'];
+    }
+
+    // app密匙
+    public function get_wx_appSecret()
+    {
+        return $this->_config[self::WX_APP_ID_INFO]['appSecret'];
+    }
+
+    // 请求发送模板需要的 access token 地址
+    public function get_wx_send_tmp_token_url()
+    {
+        return $this->_config[self::WX_REQUEST_URL]['access_token'];
+    }
+
+    // 获取模板地址
+    public function get_wx_send_tmp_url()
+    {
+        return $this->_config[self::WX_REQUEST_URL]['template'];
+    }
+}
